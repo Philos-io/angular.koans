27d2f55806e316209c1458179cefb0ccefc72043
davy engone <davy@philos.io>
2016-01-27 07:56:27 +0100

    changed path in wallaby

--- wallaby.js
+++ wallaby.js
@@ -2,13 +2,13 @@ module.exports = function () {
   return {
     files: [
       'node_modules/chai/chai.js',
-      'workshops/01_controller_service_filter_directive_testing/lib/angular.js',
+      'workshops/01_controller_service_filter_directive_testing/solution/lib/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'workshops/01_controller_service_filter_directive_testing/src/*.js',
-      'workshops/01_controller_service_filter_directive_testing/test/*.js'
+      'workshops/01_controller_service_filter_directive_testing/solution/src/*.js',
+      'workshops/01_controller_service_filter_directive_testing/solution/test/*.js'
     ],
     tests: [
-      'workshops/01_controller_service_filter_directive_testing/tests/*.js'
+      'workshops/01_controller_service_filter_directive_testing/solution/tests/*.js'
     ]
   };
 };
