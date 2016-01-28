d68c442fc9b0c32d939ac31344f116d903c90220
davy engone <davy@philos.io>
2016-01-28 07:47:32 +0100

    added book details controller

---
+++ workshops/01_controller_service_filter_directive_testing/bookDetailsController.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  BookDetailsController.$inject = ['bookFactory', '$routeParams'];
+
+  function BookDetailsController(bookFactory, $routeParams){
+    bookFactory.getBook($routeParams.id)
+      .then((response)=>{
+        // this.book = response.data;
+        angular.extend(this,  response.data);
+      });
+  }
+
+  angular.module('bookstore').controller('BookDetailsController', BookDetailsController);
+})();
